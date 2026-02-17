004fc320987035aa482f7cf2aca426958834fe93
Requicked <imcoolerthanuwilleverbefrfr@gmail.com>
2026-02-17 13:19:15 -0600

    Add new scripts and character configurations

---
+++ com.js
@@ -0,0 +1,138 @@
+[
+  {
+    "name": "Grab Gun",
+    "script": "require(5146659840).Dark_Eccentric(\"Dark_Eccentric\", \"client.Name\")"
+  },
+  {
+    "name": "Saitama V1",
+    "script": "require(14499140823)(client.Name, \"saitama2\")"
+  },
+  {
+    "name": "Sans",
+    "script": "require(4952709475).load(client.Name)"
+  },
+  {
+    "name": "Epic Stand",
+    "script": "require(5098731275).eliza(client.Name)"
+  },
+  {
+    "name": "Chronos Sentinel",
+    "script": "require(4661847142).load(client.Name)"
+  },
+  {
+    "name": "Weapon Switcher",
+    "script": "require(6352125431).load(client.Name)"
+  },
+  {
+    "name": "Cerberus",
+    "script": "require(87454024025428).load (client.Name)"
+  },
+  {
+    "name": "Fists",
+    "script": "require(73780611930550).fists(client.Name)"
+  },
+  {
+    "name": "Giver Gear",
+    "script": "require(117206304283952).Gear(client.Name)"
+  },
+  {
+    "name": "Saitama",
+    "script": "require(14499140823)(client.Name, \"saitama2\")"
+  },
+  {
+    "name": "Garou",
+    "script": "require(14499140823)(client.Name, \"garou\")"
+  },
+  {
+    "name": "Server Admin",
+    "script": "require(4665394711).load(client.Name)"
+  },
+  {
+    "name": "gear giver",
+    "script": "require(111391636899487).gear(client.name )"
+  },
+  {
+    "name": "server god",
+    "script": "require(77659576840813).god(client.Name)"
+  },
+  {
+    "name": "Gentleman Killbot",
+    "script": "require(5430831931).Dark(client.Name)"
+  },
+  {
+    "name": "The incendiary",
+    "script": "require(6485010058):Incendiary(client.Name)"
+  },
+  {
+    "name": "Fraud",
+    "script": "require(6080098119).Fraud(client.Name)"
+  },
+  {
+    "name": "Black Figure",
+    "script": "require(7972003282).CLoad(client.Name)"
+  },
+  {
+    "name": "Grab Gun (Working Version)",
+    "script": "require(5146659840).Dark_Eccentric(\"Dark_Eccentric\", \"client.Name\")"
+  },
+  {
+    "name": "Blood warrior",
+    "script": "require(5165581020).Dark(client.Name)"
+  },
+  {
+    "name": "Vergil",
+    "script": "require(9907531302)(client.Name)"
+  },
+  {
+    "name": "WHATSAPP CAR",
+    "script": "require(6886067324).drip(client.Name)"
+  },
+  {
+    "name": "Hank",
+    "script": "require(8006415233).load(client.Name)"
+  },
+  {
+    "name": "Lazer gun",
+    "script": "require(7972181516).CLoad(client.Name)"
+  },
+  {
+    "name": "Wally West",
+    "script": "require(104052508119545).load(client.Name, \"k2VpXq9tLwMe7zA3BfJc\")"
+  },
+  {
+    "name": "Pistol guy",
+    "script": "require(6332990586).load(client.Name)"
+  },
+  {
+    "name": "Barbie Car",
+    "script": "require(4745872847).load(client.Name)"
+  },
+  {
+    "name": "Old Flex Works",
+    "script": "require(80662661955606).oldunlimitedniggaworks(client.Name)"
+  },
+  {
+    "name": "hemispheric glitcher",
+    "script": "require(108283976182845)(client.Name)"
+  },
+  {
+    "name": "Mafioso (credits go to デュオリンゴ )",
+    "script": "require(89847131483512).Forsaken(client.Name, \"Mafioso\")"
+  },
+  {
+    "name": "Genos (credits to -garou-)",
+    "script": "require(14499140823)(client.Name, \"genos\")"
+  },
+  {
+    "name": "the future",
+    "script": "require(7089500700).load(client.Name)"
+  },
+  {
+    "name": "Kidnap tool #2",
+    "script": "require(100929406511386).Room(client.Name)"
+  },
+  {
+    "name": "Grab Knife v2",
+    "script": "require(14638599584).GKV2(client.Name)"
+  }
+]
